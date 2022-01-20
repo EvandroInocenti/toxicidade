31eaac50aa6d36713a43cf1fe02a4aeccee9cd4e
Evandro Inocenti <inocenti@gmail.com>
2022-01-19 23:40:55 -0300

    Adicinados rotas

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,26 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Post;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index(){
+
+        $posts = Post::get();
+
+        //dd($posts);
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    public function create(){
+        return view('admin.posts.create');
+    }
+
+    public function store(){
+        dd('cadastro de um novo post');
+    }
 }
