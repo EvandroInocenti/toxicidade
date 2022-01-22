7d4f6d861eaa4f5693a25e0217fb44c7916d3e30
Evandro Inocenti <inocenti@gmail.com>
2022-01-22 20:25:28 -0300

    adicionado função de visualiza e deletar registros

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,10 +1,15 @@
 <a href="{{ route('posts.create') }}" >criar novo post</a>
 <hr>
+@if (session('message'))
+    <div>
+        {{ session('message')}}
+    </div>
+@endif
 <h4>Posts <h4>
 
-@foreach ($posts as $post)
+    @foreach ($posts as $post)
     <p>
         {{ $post->title }}
-            [<a href="{{ route('posts.show', $post->id) }}">Ver</a>]
+        [ <a href="{{ route('posts.show', $post->id) }}">Ver </a>]
     </p>
 @endforeach
