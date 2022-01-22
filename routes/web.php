16c664eee2364844c05cad3141c28a0aeccea324
Evandro Inocenti <inocenti@gmail.com>
2022-01-22 14:36:33 -0300

    Adicinado proteção de acesso as rotas somente quando autenticado

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\{
 };
 use Illuminate\Support\Facades\Route;
 
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/create', [postController::class, 'create'])->name('posts.create');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::middleware(['auth'])->group(function () {
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/create', [postController::class, 'create'])->name('posts.create');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
