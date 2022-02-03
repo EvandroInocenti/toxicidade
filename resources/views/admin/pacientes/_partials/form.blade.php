b6f7f47a03e04ca5210ffea37f8420b7f3094853
Evandro Inocenti <inocenti@gmail.com>
2022-02-02 23:36:53 -0300

    Addd table in index

--- resources/views/admin/pacientes/_partials/form.blade.php
+++ resources/views/admin/pacientes/_partials/form.blade.php
@@ -27,59 +27,59 @@
                 Email inválido.
             </p>
         </label>
-        
+
         <label class="block">
             <span class="block text-sm font-medium text-slate-700">Senha</span>
             <input class="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" type="password" name="senha" id="senha" placeholder="Senha" value={{ $paciente->senha ?? old('senha') }}>
             <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
                 Senha.
-            </p>   
+            </p>
         </label>
-        
+
         <label class="block">
             <span class="block text-sm font-medium text-slate-700">Telefone</span>
             <input class="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" type="text" name="telefone" id="telefone" placeholder="Telefone" value={{ $paciente->telefone ?? old('telefone') }}>
-            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm"> 
+            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm">
                 Telefone
             </p>
         </label>
-        
+
         <label class="block">
             <span class="block text-sm font-medium text-slate-700">Data de nascimento</span>
             <input class="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" type="date" name="nascimento" id="nascimento" placeholder="Data de nascimento" value={{ $paciente->nascimento ?? old('nascimento') }}>
-            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm"> 
+            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm">
                 Data de nascimento
             </p>
         </label>
-            
+
         <label class="block">
             <span class="block text-sm font-medium text-slate-700">Altura</span>
             <input class="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" type="text" name="altura" id="altura" placeholder="Altura" value={{ $paciente->altura ?? old('altura') }}>
-            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm"> 
+            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm">
                 Altura
             </p>
         </label>
-        
+
         <label class="block">
             <span class="block text-sm font-medium text-slate-700">Peso</span>
             <input class="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" type="text" name="peso" id="peso" placeholder="Peso" value={{ $paciente->peso ?? old('peso') }}>
-            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm"> 
+            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm">
                 Peso
             </p>
         </label>
-        
+
         <label class="block">
             <span class="block text-sm font-medium text-slate-700">Superfície corpórea</span>
             <input class="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" type="text" name="superficie_corporea" id="superficie_corporea" placeholder="Superfície corporea" value={{ $paciente->superficie_corporea ?? old('superficie_corporea') }}>
-            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm"> 
+            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm">
                 Superfície corpórea
             </p>
          </label>
-        
+
         <label class="block">
             <span class="block text-sm font-medium text-slate-700">Acesso</span>
             <input class="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" type="text" name="acesso" id="acesso" placeholder="Acesso" value={{ $paciente->acesso ?? old('acesso') }}>
-            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm"> 
+            <p class="mt-2 invisible peer-invalid:invisible text-pink-600 text-sm">
                 Acesso
             </p>
         </label>
--- resources/views/admin/pacientes/index.blade.php
+++ resources/views/admin/pacientes/index.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Listagem dos Pacientes')
 
 @section('content')
+
 <a href="{{ route('pacientes.create') }}" >Novo paciente</a>
 <hr>
 @if (session('message'))
@@ -11,23 +12,45 @@
     </div>
 @endif
 
-<form action="{{ route('pacientes.search') }}" method="post">
-    @csrf
-    <input type="text" name="search" placeholder="Filtrar:">
-    <button type="submit">Filtrar</button>
-</form>
+<div class="p-5 mx-auto columns-2 gap-4">
+    <form action="{{ route('pacientes.search') }}" method="post">
+        @csrf
+        <label class="relative block">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+            <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><!-- ... --></svg>
+            </span>
+            <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Pesquisar paciente" type="text" name="search"/>
+        </label>
+            <button class="bg-sky-600 hover:bg-sky-700 rounded-lg h-9 w-32 text-white" type="submit">Filtrar</button>
 
-<h4>Pacientes <h4>
+    </form>
+</div>
 
-    @foreach ($pacientes as $paciente)
-    <p>
-        {{ $paciente->nome }}
-        [
-            <a href="{{ route('pacientes.show', $paciente->id) }}">Ver </a> |
-            <a href="{{ route('pacientes.edit', $paciente->id) }}">Edit </a>
-        ]
-    </p>
-@endforeach
+<div class="p-5 mx-auto columns-1">
+    <table class="table-auto bg-sky-100" >
+        <thead>
+        <tr class="bg-sky-300 text-left">
+            <th class="w-8" >ID </th>
+            <th class="w-80">Nome</th>
+            <th class="w-32">CPF</th>
+            <th class="w-80">Email</th>
+            <th class="w-32">Telefone</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($pacientes as $paciente)
+            <tr class="text-left">
+                <td >{{ $paciente->id }}</td>
+                <td >{{ $paciente->nome }}</td>
+                <td >{{ $paciente->CPF }}</td>
+                <td >{{ $paciente->email }}</td>
+                <td >{{ $paciente->telefone }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
 
 <hr>
     @if (isset($filters))
