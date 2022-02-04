dd311cfb3a66eea57c3cfab3c5dbb7ce044f1b60
Evandro Inocenti <inocenti@gmail.com>
2022-02-04 12:59:43 -0300

    Ajuste Layout

--- resources/views/admin/pacientes/show.blade.php
+++ resources/views/admin/pacientes/show.blade.php
@@ -17,7 +17,7 @@
 
     <div class="flex space-x-4 mb-5 text-sm font-medium bg-sky-50 aspect-square">
         <div class="flex-auto flex space-x-4">
-          <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3"> Excluir</button>
+            <button class="inline-flex items-center px-4 border border-transparent bg-sky-600 hover:bg-sky-700 font-semibold rounded-lg h-8 w-32 text-white tracking-widest focus:outline-none focus:border-sky-900 focus:ring ring-sky-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3" type="submit">Excluir</button>
         </div>
       </div>
 </form>
