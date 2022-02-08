09ae38bbd6dd531d3feeda6916875d49e52197b8
Evandro Inocenti <inocenti@gmail.com>
2022-02-07 21:54:38 -0300

    add layout table

--- resources/views/admin/pacientes/index.blade.php
+++ resources/views/admin/pacientes/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <header class="space-y-4 p-5 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
     <div class="flex items-center justify-between">
-        <a href="{{ route('pacientes.create') }}" class="hover:bg-blue-800 group flex items-center rounded-md bg-blue-700 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
+        <a href="{{ route('pacientes.create') }}" class="hover:bg-blue-800 group flex focus:ring-4 focus:ring-blue-300 font-medium bg-blue-700 text-white rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
                 <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
             </svg>
@@ -19,65 +19,83 @@
         @endif
     </div>
     <form class="group relative" action="{{ route('pacientes.search') }}" method="post">
-            @csrf
-            <div class="grid xl:grid-cols-2 xl:gap-6">
-                <div class="relative z-0 mb-6 w-full group">
-                    <svg width="20" height="20" fill="currentColor" class="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
-                    </svg>
-                <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Pesquisar paciente" type="text" name="search"/>
+        @csrf
+        <div class="flex flex-col">
+            <div class="grid xl:grid-cols-2 xl:gap-1">
+                <div class="relative z-0 mb-6 group">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Procurar pacientes" name="search"/>
                 </div>
                 <div class="relative z-0 mb-6 w-full group">
                     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Pesquisar</button>
                 </div>
             </div>
+        </div>
     </form>
 </header>
 
-<div class="p-5 mx-auto columns-1 ">
-    <table class="table-auto bg-sky-100" >
-        <thead>
-        <tr class="bg-sky-300 text-left">
-            <th class="w-8" >ID </th>
-            <th class="w-80">Nome</th>
-            <th class="w-32">CPF</th>
-            <th class="w-80">Email</th>
-            <th class="w-32">Telefone</th>
-            <th class="w-24"></th>
-        </tr>
-        </thead>
-        <tbody>
-            @foreach ($pacientes as $paciente)
-            <tr class="text-left">
-                <td>{{ $paciente->id }}</td>
-                <td>{{ $paciente->nome }}</td>
-                <td>{{ $paciente->CPF }}</td>
-                <td>{{ $paciente->email }}</td>
-                <td>{{ $paciente->telefone }}</td>
-                <td>
-                    <div>
-                        <span class="inline-flex items-center justify-center p-2 bg-indigo-900 rounded-md shadow-lg">
-                          <svg class="h-2 w-2 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><!-- ... --></svg>
-                          <a href="{{ route('pacientes.show', $paciente->id) }}">Ver </a>
-                        </span>
-
-                        <span class="inline-flex items-center justify-center p-2 bg-indigo-900 rounded-md shadow-lg">
-                            <svg class="h-2 w-2 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><!-- ... --></svg>
-                            <a href="{{ route('pacientes.edit', $paciente->id) }}">Edit </a>
-                          </span>
-                      </div>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+<div class="flex flex-col">
+    <div class="overflow-x-auto shadow-md sm:rounded-lg">
+        <div class="inline-block min-w-full align-middle">
+            <div class="p-4">
+                <div class="overflow-hidden">
+                    <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                        <thead class="bg-gray-100 dark:bg-gray-700">
+                            <tr>
+                                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                    Id
+                                </th>
+                                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                    Nome
+                                </th>
+                                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                    CPF
+                                </th>
+                                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                    Email
+                                </th>
+                                <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                    Telefone
+                                </th>
+                                <th scope="col" class="p-4">
+                                    <span class="sr-only">Ver</span>
+                                </th>
+                                <th scope="col" class="p-4">
+                                    <span class="sr-only">Editar</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                            @foreach ($pacientes as $paciente)
+                            <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $paciente->id }}</td>
+                                <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">{{ $paciente->nome }}</td>
+                                <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $paciente->CPF }}</td>
+                                <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $paciente->email }}</td>
+                                <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $paciente->telefone }}</td>
+                                <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                    <a href="{{ route('pacientes.show', $paciente->id) }}" class="text-blue-600 dark:text-blue-500 hover:underline">Ver</a>
+                                </td>
+                                <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                    <a href="{{ route('pacientes.edit', $paciente->id) }}" class="text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-
-<hr>
-    @if (isset($filters))
-        {{ $pacientes->appends()->links() }}
-    @else
-        {{ $pacientes->links() }}
-    @endif
+@if (isset($filters))
+{{ $pacientes->appends()->links() }}
+@else
+{{ $pacientes->links() }}
+@endif
 @endsection
